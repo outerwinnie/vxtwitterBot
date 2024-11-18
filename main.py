83cb8f934096dafb94bce4f4eec6e9ac308d11c1
Winnie <outerwinnie@protonmail.com>
2024-11-18 23:53:12 +0100

    Regex fixes

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ from logger import logger
 
 # Load configuration from environment variables
 DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
-REPLY_TO = int(os.getenv('REPLY_TO', 0))  # Default to 0 if not set
 DELETE_OP = int(os.getenv('DELETE_OP', 0))  # Default to 0 if not set
 PREAMBLE = os.getenv('PREAMBLE', '')
 TWITTER_MATCH = os.getenv('TWITTER_MATCH', '')
@@ -32,11 +31,11 @@ async def on_message(message: discord.Message) -> None:
     twitter_link = re.findall('https://twitter.com/[a-zA-Z0-9_]*/status/([0-9]+)', message.content)
     x_link = re.findall('https://x.com/[a-zA-Z0-9_]*/status/([0-9]+)', message.content)
     instagram_link = re.findall('https://www\.instagram\.com/p/[a-zA-Z0-9_-]+/?(\?[^/]+)?', message.content)
-    instagram_reel_link = re.findall('https?:\/\/(www\.)?instagram\.com\/reel\/[a-zA-Z0-9_-]+\/(\?igsh=[a-zA-Z0-9_-]+)?', message.content)
-    tiktok_link = re.findall('https:\/\/www\.tiktok\.com\/@[\w\.]+\/video\/\d+', message.content)
-    tiktok_vm_link = re.findall('https:\/\/vm\.tiktok\.com\/[a-zA-Z0-9]+\/', message.content)
-    bluesky_plc_link = re.findall('https:\/\/bsky\.app\/profile\/did:plc:[a-z0-9]{24}\/post\/[a-z0-9]+', message.content)
-    bluesky_link = re.findall('https:\/\/bsky\.app\/profile\/[a-zA-Z0-9.-]+\/post\/[a-zA-Z0-9]+', message.content)
+    instagram_reel_link = re.findall('https?://(www\.)?instagram\.com/reel/[a-zA-Z0-9_-]+/(\?igsh=[a-zA-Z0-9_-]+)?', message.content)
+    tiktok_link = re.findall('https://www\.tiktok\.com/@[\w.]+/video/\d+', message.content)
+    tiktok_vm_link = re.findall('https://vm\.tiktok\.com/[a-zA-Z0-9]+/', message.content)
+    bluesky_plc_link = re.findall('https://bsky\.app/profile/did:plc:[a-z0-9]{24}/post/[a-z0-9]+', message.content)
+    bluesky_link = re.findall('https://bsky\.app/profile/[a-zA-Z0-9.-]+/post/[a-zA-Z0-9]+', message.content)
 
     if twitter_link:
         logger.info(f'{message.guild.name}: {message.author} {message.content}')
