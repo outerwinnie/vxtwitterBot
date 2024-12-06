8426312bdb553646a0f6823e3714c2f4b9df9b8e
Winnie <outerwinnie@protonmail.com>
2024-12-06 20:17:40 +0100

    Disable instagram

--- main.py
+++ main.py
@@ -9,12 +9,12 @@ DELETE_OP = int(os.getenv('DELETE_OP', 0))  # Default to 0 if not set
 PREAMBLE = os.getenv('PREAMBLE', '')
 TWITTER_MATCH = os.getenv('TWITTER_MATCH', '')
 X_MATCH = os.getenv('X_MATCH', '')
-INSTAGRAM_MATCH = os.getenv('INSTAGRAM_MATCH', '')
+#INSTAGRAM_MATCH = os.getenv('INSTAGRAM_MATCH', '')
 TIKTOK_VM_MATCH = os.getenv('TIKTOK_VM_MATCH', '')
 TIKTOK_MATCH = os.getenv('TIKTOK_MATCH', '')
 #BLUESKY_MATCH = os.getenv('BLUESKY_MATCH', '')
 TWITTER_REPLACE = os.getenv('TWITTER_REPLACE', '')
-INSTAGRAM_REPLACE = os.getenv('INSTAGRAM_REPLACE', '')
+#INSTAGRAM_REPLACE = os.getenv('INSTAGRAM_REPLACE', '')
 TIKTOK_REPLACE = os.getenv('TIKTOK_REPLACE', '')
 #BLUESKY_REPLACE = os.getenv('BLUESKY_REPLACE', '')
 
@@ -30,8 +30,8 @@ async def on_message(message: discord.Message) -> None:
     # Only need to match once, message.content.replace replaces all
     twitter_link = re.findall('https://twitter.com/[a-zA-Z0-9_]*/status/([0-9]+)', message.content)
     x_link = re.findall('https://x.com/[a-zA-Z0-9_]*/status/([0-9]+)', message.content)
-    instagram_link = re.findall('https://www\.instagram\.com/p/[a-zA-Z0-9_-]+/?(\?[^/]+)?', message.content)
-    instagram_reel_link = re.findall('https?://(www\.)?instagram\.com/reel/[a-zA-Z0-9_-]+/(\?igsh=[a-zA-Z0-9_-]+)?', message.content)
+    #instagram_link = re.findall('https://www\.instagram\.com/p/[a-zA-Z0-9_-]+/?(\?[^/]+)?', message.content)
+    #instagram_reel_link = re.findall('https?://(www\.)?instagram\.com/reel/[a-zA-Z0-9_-]+/(\?igsh=[a-zA-Z0-9_-]+)?', message.content)
     tiktok_link = re.findall('https://www\.tiktok\.com/@[\w.]+/video/\d+', message.content)
     tiktok_vm_link = re.findall('https://vm\.tiktok\.com/[a-zA-Z0-9]+/', message.content)
     #bluesky_plc_link = re.findall('https://bsky\.app/profile/did:plc:[a-z0-9]{24}/post/[a-z0-9]+', message.content)
@@ -115,31 +115,31 @@ async def on_message(message: discord.Message) -> None:
         #if DELETE_OP == 1:
             #await message.delete()
 
-    if instagram_link or instagram_reel_link:
-        logger.info(f'{message.guild.name}: {message.author} {message.content}')
-        new_message = f'{message.author.mention} {PREAMBLE}{message.content.replace(INSTAGRAM_MATCH, INSTAGRAM_REPLACE)}'
-        reference_message = message.reference
-        allowed_mentions = discord.AllowedMentions(
-            everyone=message.mention_everyone,
-            users=message.mentions,
-            roles=message.role_mentions
-        )
+    #if instagram_link or instagram_reel_link:
+        #logger.info(f'{message.guild.name}: {message.author} {message.content}')
+        #new_message = f'{message.author.mention} {PREAMBLE}{message.content.replace(INSTAGRAM_MATCH, INSTAGRAM_REPLACE)}'
+        #reference_message = message.reference
+        #allowed_mentions = discord.AllowedMentions(
+            #everyone=message.mention_everyone,
+            #users=message.mentions,
+            #roles=message.role_mentions
+        #)
 
-        if reference_message:  # Check if the message is a reply to another message
-            replied_message = await message.channel.fetch_message(reference_message.message_id)
-            await message.channel.send(
-                new_message,
-                allowed_mentions=allowed_mentions,
-                reference=replied_message  # Reference the original replied-to message
-            )
-        else:
-            await message.channel.send(
-                new_message,
-                allowed_mentions=allowed_mentions
-            )
+        #if reference_message:  # Check if the message is a reply to another message
+            #replied_message = await message.channel.fetch_message(reference_message.message_id)
+            #await message.channel.send(
+                #new_message,
+                #allowed_mentions=allowed_mentions,
+                #reference=replied_message  # Reference the original replied-to message
+            #)
+        #else:
+            #await message.channel.send(
+                #new_message,
+                #allowed_mentions=allowed_mentions
+            #)
 
-        if DELETE_OP == 1:
-            await message.delete()
+        #if DELETE_OP == 1:
+            #await message.delete()
 
     if tiktok_link:
         logger.info(f'{message.guild.name}: {message.author} {message.content}')
